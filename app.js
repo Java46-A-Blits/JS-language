932707662dd88a451e7ff5c30a2e03416d27b393
Alex Blits <blits.alex@gmail.com>
2022-02-03 14:28:04 +0200

    variables + HW10

---
+++ app.js
@@ -0,0 +1,33 @@
+// console.log("Hello world !!!");
+// OUtput for VAR is 3 3 3 
+// for(var i=0; i<3; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+
+// output for VAR is 3 3 3 
+// for(let i=0; i<3; i++){}
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+// console.log(`a`,`${"12"-"a"}` ,`as`)
+
+// // console.log("ab"-12);       
+// console.log("12"+12,'"12"+12')
+console.log(`+"12"+12 = ${+"12"+12}`)
+// console.log(`a`,`${12-"a"}` ,`as`)
+// console.log(`a`,`${+'a'}` ,`as`)
+// console.log("ab"-12);
+// console.log(this)
+
+
+
+// function test(a){
+//     return function f(b,c){
+//         return a*(b+c);
+//     };
+// }
+//  console.log( test(5)(10, 2) );
+
