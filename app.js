ea87e9332ea8d91229bfec2e1e4ff61594742bdc
Alex Blits <blits.alex@gmail.com>
2022-02-12 16:51:03 +0200

    CW14+HW13+HW14

--- app.js
+++ app.js
@@ -1,79 +1,89 @@
-//////////// ***   ARRAYS   *** \\\\\\\\\\\\\
-const ar = [2,100,9,80];
-ar.sort(function(a,b){
-    return a-b; 
-});
-ar.sort((a,b) => a-b);
-console.log(`array is ${ar.toString()}`);
-
-// ADDING ELEMENTS TO  THE END OF ARRAY
-
-// ar[ar.length]=200;
+// const ar  = [-10, 50, -12, 80, 40];
+// ar.push(70);
+// -10#50#12#80#40#70#
+// ------    SOLUTION BASED ON SUBSTRING ----------\\\\
 // console.log(ar);
-// ar.push(300, 25);
-// console.log(ar);
-// const ar1 = [-8,30,-57];
-// ar.push(ar1);  there will be added array but not separated numbers
-// we apply spread operator " ... " to separate array elements. 
-// ar.pu.log(`after push ar1 [-8,30,-57]  ${ar}`);
-// WE APPLY  TO ADD AN ELEMENT TO  THE BEGINNING "unshift"
-// const ar2 = [27,35]
-// ar.unshift(...ar2); // the "..." spread operator works the same as before
-// console.log(` after "spread" ar2 and "unshift ar", ar now ${ar}`);
-//
-// method splice for inserting or replacing any  elements anywhere
-// 1'st argument - getting index for insert or relacement. 
-// 2'd  argument - element number of the elements to  be deleted fron the same index (included)
-//     (if 0 the slpice applied just for insert)
-// 3'd  argument - an element itself. 
-// ar.splice(2,0,1,2)
-// console.log(`after splice "1,2," from the 2'd index  ${ar}`)
-//REMOVING ELEMENTS
-// let el = ar.pop(); // removes the last element of array and returns it
-// console.log(`"pop" - removed element is ${el},  and array  now ${ar}`)
-// to  remove element from beginning we use shift 
-// el = ar.shift()
-// console.log(`"shift" - 1'st element ${el} was removed and array now ${ar}`)
-// to  remove 2,100,9,80,200
-// ar.splice(3,5)
-// console.log(ar) // removes 5 elements from index3 (included)
-//includes,  indexOf, 
-// 2D arrays
-// const matrix1 = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-// const matrix2 = [[100,20],[50,-5],[34,5]]
+// let str = '';
+// ar.forEach( function(n){
+//     str+= n+'#';
+// })
+// str = str.substring(0, str.length-1);
 
-//     function displayMatrixTr(matrix){
-//         for(let i=0; i<matrix[i].length; i++){
-//             let row = '';
-//             for (let j=0; j<matrix.length; j++){
-//                 row = row + matrix[j][i] + '  ';
-//             }
-//             console.log(row);
-//         }
+// -----  SOLUTION BASED ON forEach FROM SECOND NUMBER  ----- \\\
+
+// const ar1 = ar.slice(1); // greates array [ 50, -12, 80, 40]
+// let str =  ''+ar[0];
+// ar1.forEach(n => str += '#' + n )
+// console.log(str)
+
+// ------ PRINTNG  NUMBER OF ELEMENT, ELEMENT, LENGTH OF ARRAY ------ \\
+// 1 of 5 -10; 2 of 5 50; ......
+
+// ar.forEach((n,i,a) => console.log(`${i+1} of ${a.length} - ${n}`))
+
+// *******  mapper : 
+
+// const ar2 = ar.map(n => n*2);
+// console.log(ar2)
+
+/// *************   HW 13 task1   -  write function myForEach(array, callback-function),  where: 
+// array is iterated
+//callback  functon which is called for each element of an array
+//callback-function should take three arguments: 1) current elemet 2) current index 3) iteraed array
+// ex: myForEach(array, n=> str += '#' + n);
+
+/// ****************    HW-12    ********************\\
+
+// const ar  = [-10, 50, -12, 80, 40];
+// function mapper (n, i, a){
+//     //  console.log (`<li> ${i+1} of ${a.length} is ${n} </li>`);
+//     return `<li> ${i+1} of ${a.length} is ${n} </li>`  // ?????????????????
+// }
+// function mapper1 (n){
+//     return n + 10;
+// }
+// function myForEach(array, callback2){
+//     for( let i=0; i < array.length; i++){
+//         callback2(array[i], i, array);
 //     }
-
-//     function displayMatrix(matrix){
-//         for(let i=0; i<matrix.length; i++){
-//             let row = '';
-//             for (let j=0; j<matrix[i].length; j++){
-//                 row = row + matrix[i][j] + '  ';
-//             }
-//             console.log(row);
-//         }
+// }
+// function myMap(array, callback1){
+//  const res = [];
+//     function forEachCall(n,i,a){
+//         res.push(callback1(n,i,a))
 //     }
-// displayMatrix(matrix1);
-// displayMatrixTr(matrix1);
-// displayMatrix(matrix2);
-// displayMatrixTr(matrix2)
+//     myForEach(array, forEachCall);
+//  return res;        
+// }
+// // myForEach(ar, mapper);
+// const ar1 = myMap(ar, mapper);
+// console.log(ar1);
+// const ar2 = myMap(ar, mapper1);
+// console.log(ar2);
 
-///////////// COMPARATOR \\\\\\\\\
+// ******************   CW-14     ***********************\\
 
-// ******* Hw solution task  1
+const ar20 = [13, 17, 20, 23, 25];
+// const arEven = ar20.filter((n, i, a)=> a.length % 2 == 0 ? n % 2 ==0 : n % 2 == 1 );
+// console.log(arEven);
 
-const array1  = [13, -28, -15, -11, 12, -45, 35, 15, 22];
-const array2 =  [13, 28, 4, 15, 25, -10, 40, 17,27];
+// const arIndex  = ar20.map((_n,i,_a) => i); /// why I cound'nt do  it with ".filter" ?
+// console.log(arIndex);
 
-const comparator = (a,b) => {return  a%2==0 ? (b%2==0 ? a-b : -1) : (b%2==0 ? 1 : b-a )}
+/******************************************HW 14 definition task1 */
+//write myFilter(array, callback) based on myForEach
+//callback - function with three possible parameters: current element, current index, reference to array
+/********************************************************************* */
+ 
+const res = ar20.reduce((res,cur) => res + cur ,0); // ( res != res) , res accumulates the sum of an array
+console.log(res)
 
-console.log(array1.sort(comparator));
-console.log(array2.sort(comparator));
+const max = ar20.reduce((max, cur) => cur > max ? cur: 0 );
+console.log(max)
+
+/******************************************HW 14 definition task2 */
+//write myReduce(array, callback, initialResult) based on myForEach
+//callback - function with three possible parameters: current element, current index, reference to array
+// if the user call doesn't contain a second argument, then the first elemnt of the array will be
+//considered as initial result (in this case iterating begins from the second element of the array)
+
