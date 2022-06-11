a38fd291a2024a21639630a825ffaedf9d1af4b8
Alex Blits <blits.alex@gmail.com>
2022-06-11 19:44:11 +0300

    notes added

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const ar = [2, 100, 9, 80];
 // ar.sort(function (a, b){
 //         return a - b;
 // });
-// ar.sort((a,b) => a - b)
-// console.log(`ar is ${ar.toString()}`);
+ar.sort((a,b) => a - b)
+console.log(`ar is ${ar.toString()}`);
 
 //adding element/s at end of an array
 ar[ar.length] = 200;
 console.log(ar);
 ar.push(300, 25);
-console.log(ar);
-const ar1 = [-8, 30, -57];
+console.log('after push',ar);
+const ar1 = [-8, 30, -57]; 
 // ar.push(ar1); there will be added array but not separated numbers
-// if you want to add separated numbers you should apply a spread operator ...
+// if you want to add separated numbers you should apply a spread operator "..."
 ar.push(...ar1);
 console.log(ar); 
 //for adding elements at beginning of an array there is method "unshift"
@@ -21,22 +21,21 @@ console.log(ar);
 //adds elements at beginning
 const ar2 = [27, 35];
 ar.unshift(...ar2);
-console.log(ar);
-//abc123drtyugr
+console.log('after unshift',ar);
 //method splice for inserting or replacing any elements in any place
 //first argument - index for inserting or replacing
 //second argument - number of the deleted elements (if 0 than splice will be applied only for inserting)
 //third argiment designates sequence of being inserted element like push/unshift
 ar.splice(2,0,1,2);
-console.log(ar);
+console.log('splice',ar);
 //removing (deleting)
 let el = ar.pop(); //removes the last element and returns it
 //el = -57; -57 will be removed from the array ar
 el = ar.shift(); //removes the first element and returns it
 //el = 27; 27 will be removed from the array ar
 console.log(el, ar);
-ar.splice(3, 5); //remove 5 elements beginning from the one at index 3
-console.log(ar);
+ar.splice(3, 5); //remove 5 elements beginning from the index 3
+console.log('ar.splice(3, 5)',ar);
 //includes, indexOf
 //two dimensional arrays
 const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 4, 5]];
@@ -51,6 +50,7 @@ function displayMatrix(matrix) {
      }  
 }
 //displayMatrix(matrix1);
+console.log('matrix2',matrix2);
 displayMatrix(matrix2);
 /******************HW #12 definition */
 //const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
@@ -70,7 +70,7 @@ displayMatrix(matrix2);
 
 /************************************HW# 12 Solution task 1*/
 const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
-//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13]
+//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13] - even ascending, odd  descending
 const comparator = (n1, n2) => {
         let res = n1 % 2 - n2 % 2;
         if (res == 0) {
